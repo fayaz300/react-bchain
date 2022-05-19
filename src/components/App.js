8fe471919d3a94d686a882a2a6c8fc5cbd384ca1
fayaz <fayazalem24@gmail.com>
2022-05-19 13:49:03 +0530

    Base App

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="ui container">
+      <h1>Hello world</h1>
+      <div className='ui item'>
+        <button className='ui button positive'>Click Me</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
